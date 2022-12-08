9201b1f2402f2d5ab38342bf91fdaa8ee2bc9564
Alessandro Belli <alessandrobelli90@gmail.com>
2022-12-08 19:23:02 +0100

    now you add dates and year property to months, and weeks can get icons as url or emoji

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
 const readline = require("readline");
-const { Client } = require("@notionhq/client")
-const dotenv = require("dotenv")
+const { Client } = require("@notionhq/client");
+const dotenv = require("dotenv");
 const moment = require("moment");
 const { abort } = require("process");
 const { months } = require("moment");
 
-dotenv.config()
+dotenv.config();
 
-const notion = new Client({ auth: process.env.NOTION_KEY })
+const notion = new Client({ auth: process.env.NOTION_KEY });
 
-const databaseId = process.env.NOTION_DATABASE_WEEKS
-const databaseIdMonths = process.env.NOTION_DATABASE_MONTHS
-const iconToAdd = process.env.ICON
-var start = moment('2022-01-03 00:00', "YYYY-MM-DD HH:mm");
-var end = moment('2022-12-31 00:00', "YYYY-MM-DD HH:mm");
-let weekBefore = { "id": process.env.LAST_WEEK_2021_ID }
-let actualMonth = { "id": "" };
-let monthBefore = { "id": "" };
+const databaseId = process.env.NOTION_DATABASE_WEEKS;
+const databaseIdMonths = process.env.NOTION_DATABASE_MONTHS;
+const databaseIdYear = process.env.DATABASE_YEARS_ID;
+const iconURL = process.env.ICON;
+const emoji = process.env.EMOJI;
+var start = moment("2023-01-02 00:00", "YYYY-MM-DD HH:mm");
+var end = moment("2023-12-31 00:00", "YYYY-MM-DD HH:mm");
+let weekBefore = { id: process.env.LAST_WEEK_2021_ID };
+let actualMonth = { id: "" };
+let monthBefore = { id: "" };
 populateDB(start, end, weekBefore);
 
 async function populateDB(start, end, weekBefore) {
-
-  for (var m = moment(start); m.isBefore(end); m.add(7, 'days')) {
-    monthBefore = await getMonth(m.format("MMMM"), m.format("YYYY"));
-    actualMonth = await getMonth(m.clone().add(6, 'd').format("MMMM"), m.clone().add(6, 'd').format("YYYY"));
-    weekBefore = await fillWeeks(m, weekBefore.id, monthBefore.results[0], actualMonth.results[0]);
-    console.log("Filling " + m.format("MMMM") + " ...")
+  let i = 1;
+  for (var m = moment(start); m.isBefore(end); m.add(7, "days")) {
+    monthBefore = await getMonth(m.format("MMMM"), m.format("YYYY"), m.clone());
+    actualMonth = await getMonth(
+      m.clone().add(6, "d").format("MMMM"),
+      m.clone().add(6, "d").format("YYYY"),
+      m.clone().add(6, "d")
+    );
+    weekBefore = await fillWeeks(
+      m,
+      weekBefore.id,
+      monthBefore.results[0],
+      actualMonth.results[0]
+    );
+    console.log("Filling week " + i + " of " + m.format("MMMM") + " ...");
+    if (m.format("MMM") === m.clone().add(6, "d").format("MMM")) i++;
+    else i = 1;
   }
-
-
-
 }
 
 async function fillWeeks(momentName, weekBefore, monthBefore, actualMonth) {
-
   let createWeekPage = {};
   createWeekPage.parent = {
     database_id: databaseId,
   };
-  if (iconToAdd !== "") {
+  if (iconURL !== "") {
     createWeekPage.icon = {
       type: "external",
       external: {
-        url: iconToAdd
-      }
+        url: iconURL,
+      },
     };
-  }
-  if (process.env.WEEK_BEFORE_NAME !== "") {
-    createWeekPage.properties[process.env.WEEK_BEFORE_NAME] = {
-      relation: [{
-        "id": weekBefore
-      }]
+  } else if (emoji !== "") {
+    createWeekPage.icon = {
+      emoji: emoji,
     };
   }
 
@@ -59,60 +65,112 @@ async function fillWeeks(momentName, weekBefore, monthBefore, actualMonth) {
       title: [
         {
           text: {
-            content: (momentName.format("MMM") === momentName.clone().add(6, 'd').format("MMM")) ? momentName.format('D') + " - " + momentName.clone().add(6, 'd').format('D') + " " + momentName.clone().add(6, 'd').format("MMM") : momentName.format('D') + " " + momentName.format("MMM") + " - " + momentName.clone().add(6, 'd').format('D') + " " + momentName.clone().add(6, 'd').format("MMM"),
+            content:
+              momentName.format("MMM") ===
+              momentName.clone().add(6, "d").format("MMM")
+                ? momentName.format("D") +
+                  " - " +
+                  momentName.clone().add(6, "d").format("D") +
+                  " " +
+                  momentName.clone().add(6, "d").format("MMM")
+                : momentName.format("D") +
+                  " " +
+                  momentName.format("MMM") +
+                  " - " +
+                  momentName.clone().add(6, "d").format("D") +
+                  " " +
+                  momentName.clone().add(6, "d").format("MMM"),
           },
         },
       ],
     },
     [process.env.DATES_NAME]: {
-      date:
-      {
-        "start": momentName.toISOString(true),
-        "end": momentName.clone().add(6, 'd').toISOString(true)
+      date: {
+        start: momentName.toISOString(true),
+        end: momentName.clone().add(6, "d").toISOString(true),
       },
-
     },
     [process.env.MONTH_NAME]: {
-      relation: [{
-        "id": monthBefore.id
-      }, {
-        "id": actualMonth.id
-      }]
-    }
+      relation: [
+        {
+          id: monthBefore.id,
+        },
+        {
+          id: actualMonth.id,
+        },
+      ],
+    },
   };
+
+  if (process.env.WEEK_BEFORE_NAME !== "") {
+    createWeekPage.properties[process.env.WEEK_BEFORE_NAME] = {
+      relation: [
+        {
+          id: weekBefore,
+        },
+      ],
+    };
+  }
 
   return await notion.pages.create(createWeekPage);
 }
 
-async function getMonth(monthName, year) {
-  const databaseId = databaseIdMonths;
+async function getMonth(monthName, year, momentName) {
+  const databaseIdM = databaseIdMonths;
+  const databaseIdY = databaseIdYear;
   let month = await notion.databases.query({
-    database_id: databaseId,
+    database_id: databaseIdM,
     filter: {
-      "property": "Name",
-      "text": {
-        "contains": monthName + " " + year
-      }
-    }
+      property: "Name",
+      text: {
+        contains: monthName + " " + year,
+      },
+    },
+  });
+  let yearArray = await notion.databases.query({
+    database_id: databaseIdY,
+    filter: {
+      property: "Name",
+      text: {
+        contains: year.toString(),
+      },
+    },
   });
 
-  if (month.results.length === 0) {
-    month.results.push(await notion.pages.create({
-      parent: {
-        database_id: databaseIdMonths
-      },
-      properties: {
-        Name: {
-          title: [
-            {
-              text: {
-                content: monthName + " " + year,
-              },
+  let objectMonth = {
+    parent: {
+      database_id: databaseIdMonths,
+    },
+    properties: {
+      Name: {
+        title: [
+          {
+            text: {
+              content: monthName + " " + year,
             },
-          ],
+          },
+        ],
+      },
+      [process.env.DATES_NAME]: {
+        date: {
+          start: momentName.startOf("month").toISOString(true),
+          end: momentName.endOf("month").toISOString(true),
         },
-      }
-    }));
+      },
+    },
+  };
+  if (process.env.YEAR_NAME !== "") {
+    objectMonth.properties[process.env.YEAR_NAME] = {
+      relation: [
+        {
+          id: yearArray.results[0].id,
+        },
+      ],
+    };
+  }
+
+  if (month.results.length === 0) {
+    month.results.push(await notion.pages.create(objectMonth));
   }
 
   return month;
